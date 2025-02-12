501872c16f1c8617d8fe422c609309dd124d52d6
Abdalrahman-Amin <abdulrahman.amin@oceanbluesoftware.co.uk>
2025-02-12 15:38:02 +0200

    add img domains

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
    /* config options here */
    images: {
-      domains: ["qaufbqytrhwgeteijboz.supabase.co"], // ✅ Add your Supabase domain
+      domains: ["qaufbqytrhwgeteijboz.supabase.co", "picsum.photos"], // ✅ Add your Supabase domain
    },
 };
 
