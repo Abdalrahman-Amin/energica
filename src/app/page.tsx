a37e553c79d468c297ed8b1dc43b9aa6e34742aa
Abdalrahman-Amin <abdulrahman.amin@oceanbluesoftware.co.uk>
2025-02-14 17:31:01 +0200

    add loader in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,31 @@ import React, { useState, useEffect } from "react";
 import Category from "../components/Category";
 import { Category as categoryType } from "@/types/types";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import Loader from "@/components/Loader";
 
 export default function Home() {
    const [categories, setCategories] = useState<categoryType[]>([]);
+   const [isLoading, setIsLoading] = useState(true);
    const supabase = createClientComponentClient();
 
    useEffect(() => {
       const fetchCategories = async () => {
+         setIsLoading(true);
          const { data, error } = await supabase.from("categories").select("*");
          if (error) {
             console.error("Error fetching categories:", error);
          } else {
             setCategories(data || []);
          }
+         setIsLoading(false);
       };
 
       fetchCategories();
    }, [supabase]);
+
+   if (isLoading) {
+      return <Loader />;
+   }
 
    return (
       <main className="mt-36 min-h-screen">
