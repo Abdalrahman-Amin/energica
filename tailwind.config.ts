9592b2083e8c3b349dd10c0da8494e82af59a366
Abdalrahman-Amin <abdulrahman.amin@oceanbluesoftware.co.uk>
2025-02-18 21:01:13 +0200

    try disable dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-   darkMode: "class", // or "media"
+   darkMode: undefined, // or "media"
    content: [
       "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
       "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
