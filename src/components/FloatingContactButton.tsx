37bb0bb467076625fcd7a17a4a69251cbc349054
Abdalrahman-Amin <abdulrahman.amin@oceanbluesoftware.co.uk>
2025-02-20 10:57:28 +0200

    fix dloating button position

--- src/components/FloatingContactButton.tsx
+++ src/components/FloatingContactButton.tsx
@@ -91,7 +91,7 @@ const FloatingContactButton = () => {
                   animate={{ opacity: 1, y: 0, scale: 1 }}
                   exit={{ opacity: 0, y: -20, scale: 0.95 }}
                   transition={{ duration: 0.2 }}
-                  className="absolute top-20 right-0 md:right-20 lg:right-48 xl:right-80"
+                  className="absolute top-10 right-0 "
                >
                   <Card className="p-4 w-64 shadow-lg">
                      <motion.div className="space-y-3">
